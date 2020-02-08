478a7191bf7a2bd57f49e6fda1901e37ea11546b
Noman anjum <nomananjum420@gmail.com>
2020-02-08 23:58:47 +0500

    multi image beta

--- src/app/barter/new-post/new-post.component.ts
+++ src/app/barter/new-post/new-post.component.ts
@@ -4,9 +4,9 @@ import { AuthService } from './../../services/auth.service';
 import { Post } from './../../models/post.model';
 import {AngularFireStorage,AngularFireStorageReference,AngularFireUploadTask} from 'angularfire2/storage' ;
 import { Observable} from 'rxjs/Observable';
-
-import { timestamp,finalize } from 'rxjs/operators';
-import { from } from 'rxjs';
+import { AngularFirestore, QuerySnapshot } from 'angularfire2/firestore';
+import { timestamp,finalize,tap,map } from 'rxjs/operators';
+import { from,combineLatest } from 'rxjs';
 
 
 
@@ -16,21 +16,31 @@ import { from } from 'rxjs';
   styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent implements OnInit {
+  
+  
+  uploads: any[];
+  allPercentage: Observable<any>;
+  files: Observable<any>;
+  
+  
+  
   downloadURL: Observable<string>;
   uploadProgress: Observable<number>;
-  afStorage: AngularFireStorage;
+  //afStorage: AngularFireStorage;
   ref:AngularFireStorageReference;
   task:AngularFireUploadTask;
   postTitle = '';
   postDescription = '';
   user = null;
   id = '';
-  constructor(private postcrud: PostCrudService, auth:AuthService, afStorage:AngularFireStorage) {
+  constructor(private postcrud: PostCrudService, auth:AuthService,public afStorage:AngularFirestore,public storage: AngularFireStorage) {
     auth.user$.subscribe( user => this.user = user);
-    this.afStorage=afStorage;
+    //this.afStorage=afStorage;
   }
 
   ngOnInit() {
+    this.files = this.afStorage.collection('files').valueChanges();
+
    }
   postSave(event){
     console.log('Post');
@@ -50,15 +60,60 @@ export class NewPostComponent implements OnInit {
     this.postcrud.addPost(p);
   }
   upload(event){
-    const id = Math.random().toString(36).substring(2);
-    this.ref = this.afStorage.ref(id);
-    this.task = this.ref.put(event.target.files[0]);
-    this.uploadProgress = this.task.percentageChanges();
-    //https://stackoverflow.com/questions/50541836/property-downloadurl-does-not-exist-on-type-angularfireuploadtask
-    this.task.snapshotChanges().pipe(
-      finalize(() => this.downloadURL = this.ref.getDownloadURL())
-      )
-    .subscribe();
+    this.uploads = [];
+    const filelist = event.target.files;
+    const allPercentage: Observable<number>[] = [];
+
+    for (const file of filelist) {
+
+      const path = `files/${file.name}`;
+      const ref = this.storage.ref(path);
+      const task = this.storage.upload(path, file);
+      const _percentage$ = task.percentageChanges();
+      allPercentage.push(_percentage$);
+
+      // create composed object with different information. ADAPT THIS ACCORDING YOUR NEED
+      const uploadTrack = {
+        fileName: file.name,
+        percentage: _percentage$
+      }
+
+      // push each upload into the array
+      this.uploads.push(uploadTrack);
+
+      // for every upload do whatever you want in firestore with the uploaded file
+      const _t = task.then((f) => {
+        return f.ref.getDownloadURL().then((url) => {
+          return this.afStorage.collection('files').add({
+            name: f.metadata.name,
+            url: url
+          });
+        })
+      })
+
+    }
+
+    this.allPercentage = combineLatest(allPercentage)
+      .pipe(
+      map((percentages) => {
+        let result = 0;
+        for (const percentage of percentages) {
+          result = result + percentage;
+        }
+        return result / percentages.length;
+      }),
+      tap(console.log)
+      );
+
+    // const id = Math.random().toString(36).substring(2);
+    // this.ref = this.afStorage.ref(id);
+    // this.task = this.ref.put(event.target.files[0]);
+    // this.uploadProgress = this.task.percentageChanges();
+    // //https://stackoverflow.com/questions/50541836/property-downloadurl-does-not-exist-on-type-angularfireuploadtask
+    // this.task.snapshotChanges().pipe(
+    //   finalize(() => this.downloadURL = this.ref.getDownloadURL())
+    //   )
+    // .subscribe();
     
   }
   postTitleChange(event){
